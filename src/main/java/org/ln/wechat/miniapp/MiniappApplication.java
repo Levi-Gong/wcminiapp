a2400e3bfcf9ec5ccd7e65f01eba4d8592a13d71
Levi-Gong <levi.gong@sinoservices.com>
2019-03-20 00:09:45 +0800

    jms activemq emb

--- src/main/java/org/ln/wechat/miniapp/MiniappApplication.java
+++ src/main/java/org/ln/wechat/miniapp/MiniappApplication.java
@@ -3,9 +3,11 @@ package org.ln.wechat.miniapp;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
 @MapperScan("org.ln.wechat.miniapp.mapper")
+@EnableJms
 public class MiniappApplication {
 
   public static void main(String[] args) {
---
+++ src/main/java/org/ln/wechat/miniapp/config/JMSConfig.java
@@ -0,0 +1,47 @@
+package org.ln.wechat.miniapp.config;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+
+@Configuration
+public class JMSConfig {
+
+    @Value("spring.activemq.broker-url")
+    private String brokerUrl;
+    @Value("spring.activemq.user")
+    private String brokerUsername;
+    @Value("spring.activemq.password")
+    private String brokerPassword;
+
+    @Bean
+    public ActiveMQConnectionFactory connectionFactory(){
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+        connectionFactory.setBrokerURL(brokerUrl);
+        connectionFactory.setPassword(brokerUsername);
+        connectionFactory.setUserName(brokerPassword);
+        return connectionFactory;
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplate(){
+        JmsTemplate template = new JmsTemplate();
+        template.setConnectionFactory(connectionFactory());
+        template.setPubSubDomain(true);
+        return template;
+    }
+
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory());
+        factory.setConcurrency("1-1");
+        factory.setPubSubDomain(true);
+        return factory;
+    }
+
+
+}
